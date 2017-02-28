18618d78bb628a193682727abeff763e5d71c887
nurghaidafajri <nurghaida059@gmail.com>
2017-02-28 18:48:10 +0700

    Add files via upload

---
+++ Tugas 2/Mahasiswa.java
@@ -0,0 +1,7 @@
+class Mahasiswa{
+	public static void main (String argv[]){
+		System.out.println("Nama	: Nurghaida Fajriati Kurnia");
+		System.out.println("NIM	: 10115431");
+		System.out.println("Alamat	: Tubagus Ismail Bawah");
+	}
+}
